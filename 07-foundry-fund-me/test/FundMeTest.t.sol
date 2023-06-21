7a67f9aea71dda81b6d230723b30c7ab60510a93
Lulox <ligma@duck.com>
2023-06-20 22:20:16 -0300

    First commit

--- 07-foundry-fund-me/test/FundMeTest.t.sol
+++ 07-foundry-fund-me/test/FundMeTest.t.sol
@@ -20,6 +20,16 @@ contract FundMeTest is Test {
         assertEq(fundMe.i_owner(), address(this));
     }
 
+    // What can we do to work with addresses outside our system?
+    // 1. Unit
+    //     - Testing a specific part of our code
+    // 2. Integration
+    //     - Testing how our code works with other parts of our code
+    // 3. Forked
+    //     - Testing our code on a simulated real environment
+    // 4. Staging
+    //     - Testing our code in a real environment that is not prod
+
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
